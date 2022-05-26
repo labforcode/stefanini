bd416a8f454a4e7d561359afa35eeea5fca743af
labforcode <labforcode@gmail.com>
2022-05-26 20:01:27 -0300

    Criados endpoints

---
+++ src/TesteStefanini.WebApi/Controllers/CidadeController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TesteStefanini.Cadastros.Aplicacoes.Interfaces.Cidades;
+
+namespace TesteStefanini.WebApi.Controllers
+{
+    [ApiController]
+    public class CidadeController : ControllerBase
+    {
+        private readonly ICidadeAppService _cidadeAppService;
+
+        public CidadeController(ICidadeAppService cidadeAppService)
+        {
+            _cidadeAppService = cidadeAppService;
+        }
+
+        [HttpPost]
+        [Route("cidade")]
+        public IActionResult Cadastrar()
+        {
+            try
+            {
+
+
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Serviço indisponível, tente novamente mais tarde");
+            }
+        }
+
+        [HttpPut]
+        [Route("cidade")]
+        public IActionResult Atualizar()
+        {
+            try
+            {
+
+
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Serviço indisponível, tente novamente mais tarde");
+            }
+        }
+
+        [HttpGet]
+        [Route("cidade/id/{id}")]
+        public IActionResult ObterPorId(int id)
+        {
+            try
+            {
+
+
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Serviço indisponível, tente novamente mais tarde");
+            }
+        }
+
+        [HttpGet]
+        [Route("cidade/nome/{nome}")]
+        public IActionResult ObterPorNome(string nome)
+        {
+            try
+            {
+
+
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Serviço indisponível, tente novamente mais tarde");
+            }
+        }
+
+        [HttpGet]
+        [Route("cidade/uf/{uf}")]
+        public IActionResult ObterPorUf()
+        {
+            try
+            {
+
+
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Serviço indisponível, tente novamente mais tarde");
+            }
+        }
+
+        [HttpGet]
+        [Route("cidades")]
+        public IActionResult ObterTodos()
+        {
+            try
+            {
+
+
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Serviço indisponível, tente novamente mais tarde");
+            }
+        }
+    }
+}
---
+++ src/TesteStefanini.WebApi/Controllers/PessoaController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TesteStefanini.Cadastros.Aplicacoes.Interfaces.Pessoas;
+
+namespace TesteStefanini.WebApi.Controllers
+{
+    [ApiController]
+    public class PessoaController : ControllerBase
+    {
+        private readonly IPessoaAppService _pessoaAppService;
+
+        public PessoaController(IPessoaAppService pessoaAppService)
+        {
+            _pessoaAppService = pessoaAppService;
+        }
+
+        [HttpPost]
+        [Route("pessoa")]
+        public IActionResult Cadastrar()
+        {
+            try
+            {
+
+
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Serviço indisponível, tente novamente mais tarde");
+            }
+        }
+
+        [HttpPut]
+        [Route("pessoa")]
+        public IActionResult Atualizar()
+        {
+            try
+            {
+
+
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Serviço indisponível, tente novamente mais tarde");
+            }
+        }
+
+        [HttpGet]
+        [Route("pessoa/id/{id}")]
+        public IActionResult ObterPorId(int id)
+        {
+            try
+            {
+
+
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Serviço indisponível, tente novamente mais tarde");
+            }
+        }
+
+        [HttpGet]
+        [Route("pessoa/cpf/{cpf}")]
+        public IActionResult ObterPorCpf(string cpf)
+        {
+            try
+            {
+
+
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Serviço indisponível, tente novamente mais tarde");
+            }
+        }
+
+        [HttpGet]
+        [Route("pessoas")]
+        public IActionResult ObterTodos()
+        {
+            try
+            {
+
+
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Serviço indisponível, tente novamente mais tarde");
+            }
+        }
+    }
+}
